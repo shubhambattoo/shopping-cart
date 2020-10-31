850e5c07242b085e840940579681461a6faa836f
shubhambattoo <shubham.battoo@hotmail.com>
2020-10-31 19:25:58 +0530

    added notification on cart update

---
+++ client/build/precache-manifest.1c43778d3f348a5207d15aecb28d5101.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc468c3ec7285c80c49567b964b3c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd171e60af1fd9089930",
+    "url": "/static/css/3.2ecb3de9.chunk.css"
+  },
+  {
+    "revision": "a4c9cf49d26565c8823d",
+    "url": "/static/css/4.bffa568a.chunk.css"
+  },
+  {
+    "revision": "b35477d79993570b2598",
+    "url": "/static/css/5.249ba434.chunk.css"
+  },
+  {
+    "revision": "e2a3cf54f11a7c4f36c9",
+    "url": "/static/css/6.c1a64bfd.chunk.css"
+  },
+  {
+    "revision": "f5dc8538c4752cbdf10b",
+    "url": "/static/css/main.4de9d7d4.chunk.css"
+  },
+  {
+    "revision": "e4e32138611757fe6a66",
+    "url": "/static/js/2.abb6b1e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.abb6b1e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd171e60af1fd9089930",
+    "url": "/static/js/3.5dafcd24.chunk.js"
+  },
+  {
+    "revision": "e7ea479d201397994db797ad41e5885f",
+    "url": "/static/js/3.5dafcd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c9cf49d26565c8823d",
+    "url": "/static/js/4.ffa7733e.chunk.js"
+  },
+  {
+    "revision": "b35477d79993570b2598",
+    "url": "/static/js/5.9c01ca3b.chunk.js"
+  },
+  {
+    "revision": "e2a3cf54f11a7c4f36c9",
+    "url": "/static/js/6.c1b935ef.chunk.js"
+  },
+  {
+    "revision": "f5dc8538c4752cbdf10b",
+    "url": "/static/js/main.f2a08aae.chunk.js"
+  },
+  {
+    "revision": "b9cde0f5fe6f64859d3d",
+    "url": "/static/js/runtime-main.12b0e618.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
